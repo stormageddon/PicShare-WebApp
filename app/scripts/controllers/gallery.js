03f0a3edf4a524b8240c793973fa6eea3f615cb3
Michael Caputo <mcaputo@cloudmine.me>
2015-12-14 22:27:55 -0500

    Show upload date

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -9,6 +9,26 @@
  */
 angular.module('picShareApp')
   .controller('GalleryController', function ($scope, $routeParams) {
+    console.log('cloudmine:', cloudmine);
     $scope.imageid = $routeParams.imageid;
+
+    var ws = new cloudmine.WebService({
+      appid: '933cd5ae80cfc140244a4158c5558db3',
+      apikey: 'c6ee6dcbf7e8435ab90edc90fc6c704e',
+      apiroot: 'https://api.secure.cloudmine.me'
+    })
+
+    $scope.imageData = {}
+
+    ws.get($scope.imageid).on('success', function(data){
+      console.log("Got:", data);
+      $scope.imageData.createdAt = data[$scope.imageid]["__created__"]
+      $scope.$apply()
+      console.log('created:', $scope.imageData.createdAt);
+    }).on('error', function(err) {
+      console.log("error:", err);
+    });
+
+
     console.log('Image id:', $routeParams.imageid);
   });
