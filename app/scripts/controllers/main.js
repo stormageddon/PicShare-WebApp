a6b9bec07bbb391bbc26779073dd99f92dde498d
Michael Caputo <mcaputo@cloudmine.me>
2016-01-07 15:02:34 -0500

    Update main page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,17 @@
  * Controller of the picShareApp
  */
 angular.module('picShareApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $location, $window, $timeout) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    var githubBase = 'http://github.com/stormageddon/';
+
+    $scope.openGithub = function(repo) {
+      $window.location = githubBase + repo;
+    };
+
   });
